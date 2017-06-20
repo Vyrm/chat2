4a1f659f2654844acaa5670c2abb1c7f42449de0
Sergey <mymail@mymail.com>
2017-06-20 16:33:09 +0300

    task 2, change exit edits

--- src/main/java/com/devcolibri/Worker.java
+++ src/main/java/com/devcolibri/Worker.java
@@ -22,17 +22,23 @@ public class Worker implements Runnable {
     }
 
     public void run() {
+        boolean userAvailable = true;
         pw.println("Welcome to chat!");
-        while (true) {
+        while (userAvailable) {
             try {
                 String msg = bf.readLine();
-                if (msg.equals("/exit")) throw new NullPointerException();
+                if (msg.equals("/exit")) {
+                    System.out.println("The client left the chat");
+                    userAvailable = false;
+                    socket.close();
+                    break;
+                }
                 System.out.println(msg);
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (NullPointerException e) {
                 System.out.println("The client left the chat");
-                break;
+                userAvailable = false;
             }
         }
     }
