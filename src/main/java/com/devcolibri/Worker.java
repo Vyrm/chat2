973db9e5ab1dac211430ccc5cb90eed34712be98
Serhii Shepel <sshepel@softserveinc.com>
2017-06-22 13:58:47 +0300

    Revert "Merge pull request #3 from Squallman/task3"
    d:
    his reverts commit 3abc770bfb32e84b07a42ca67759ffce69ea54e2, reversing
    changes made to 862dd771a2bc26b821c151511ff174c144905c2e.

--- src/main/java/com/devcolibri/Worker.java
+++ src/main/java/com/devcolibri/Worker.java
@@ -5,60 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Map;
 
 public class Worker implements Runnable {
     private Socket socket;
     private BufferedReader bf;
     private PrintWriter pw;
-    private boolean userAvailableExit;
+    private boolean userAvailable;
 
     public Worker(Socket sck) throws IOException {
         this.socket = sck;
         bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         pw = new PrintWriter(socket.getOutputStream(), true);
-        userAvailableExit = true;
     }
 
     public void run() {
-        pw.println("Welcome to chat, enter your nickname!");
-        getNickname();
-        sendMessage();
-    }
-
-    private void getNickname() {
-        boolean userAvailable = true;
-        while (userAvailable && userAvailableExit) {
-            try {
-                String nickname = bf.readLine();
-                if (nickname == null || nickname.equals("/exit")) {
-                    userAvailable = exit();
-                } else if (UserHandler.getInstance().getMap().isEmpty()) {
-                    UserHandler.getInstance().getMap().put(socket, new User(nickname));
-                    userAvailable = false;
-                } else {
-                    for (Map.Entry<Socket, User> entry : UserHandler.getInstance().getMap().entrySet()) {
-                        if (entry.getValue().equals(new User(nickname))) { //TODO
-                            pw.println("This user is already exist, choose another");
-                        } else {
-                            UserHandler.getInstance().getMap().put(socket, new User(nickname));
-                            userAvailable = false;
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void sendMessage() {
-        boolean userAvailable = true;
-        while (userAvailable && userAvailableExit) {
+        userAvailable = true;
+        pw.println("Welcome to chat!");
+        while (userAvailable) {
             try {
                 String msg = bf.readLine();
                 if (msg == null || msg.equals("/exit")) {
-                    userAvailable = exit();
+                    exit();
                 } else {
                     System.out.println(msg);
                 }
@@ -68,12 +35,11 @@ public class Worker implements Runnable {
         }
     }
 
-    private boolean exit() throws IOException {
+    private void exit() throws IOException {
         System.out.println("The client left the chat");
-        userAvailableExit = false;
+        userAvailable = false;
         socket.close();
         bf.close();
         pw.close();
-        return false;
     }
 }
